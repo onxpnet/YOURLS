cedbbfcd31b312b902fd0d0fcbd2160223fc1dcf
Glend Maatita <glend.maatita@gmail.com>
2024-02-08 11:14:55 +0700

    update config sample

--- user/config-sample.php
+++ user/config-sample.php
@@ -11,23 +11,23 @@
  */
 
 /** MySQL database username */
-define( 'YOURLS_DB_USER', 'your db user name' );
+define( 'YOURLS_DB_USER', $_ENV["DB_USER"] );
 
 /** MySQL database password */
-define( 'YOURLS_DB_PASS', 'your db password' );
+define( 'YOURLS_DB_PASS', $_ENV["DB_PASSWORD"] );
 
 /** The name of the database for YOURLS
  ** Use lower case letters [a-z], digits [0-9] and underscores [_] only */
-define( 'YOURLS_DB_NAME', 'yourls' );
+define( 'YOURLS_DB_NAME', $_ENV["DB_NAME"] );
 
 /** MySQL hostname.
  ** If using a non standard port, specify it like 'hostname:port', e.g. 'localhost:9999' or '127.0.0.1:666' */
-define( 'YOURLS_DB_HOST', 'localhost' );
+define( 'YOURLS_DB_HOST', $_ENV["DB_HOST"] );
 
 /** MySQL tables prefix
  ** YOURLS will create tables using this prefix (eg `yourls_url`, `yourls_options`, ...)
  ** Use lower case letters [a-z], digits [0-9] and underscores [_] only */
-define( 'YOURLS_DB_PREFIX', 'yourls_' );
+define( 'YOURLS_DB_PREFIX', $_ENV["DB_PREFIX"] );
 
 /*
  ** Site options
@@ -37,33 +37,34 @@ define( 'YOURLS_DB_PREFIX', 'yourls_' );
  ** All lowercase, no trailing slash at the end.
  ** If you define it to "http://sho.rt", don't use "http://www.sho.rt" in your browser (and vice-versa)
  ** To use an IDN domain (eg http://héhé.com), write its ascii form here (eg http://xn--hh-bjab.com) */
-define( 'YOURLS_SITE', 'http://your-own-domain-here.com' );
+define( 'YOURLS_SITE', $_ENV["APP_URL"] );
 
 /** YOURLS language
  ** Change this setting to use a translation file for your language, instead of the default English.
  ** That translation file (a .mo file) must be installed in the user/language directory.
  ** See http://yourls.org/translations for more information */
-define( 'YOURLS_LANG', '' );
+define( 'YOURLS_LANG', $_ENV["LANG"] );
 
 /** Allow multiple short URLs for a same long URL
  ** Set to true to have only one pair of shortURL/longURL (default YOURLS behavior)
  ** Set to false to allow multiple short URLs pointing to the same long URL (bit.ly behavior) */
-define( 'YOURLS_UNIQUE_URLS', true );
+define( 'YOURLS_UNIQUE_URLS', $_ENV["IS_UNIQUE_URL"] );
 
 /** Private means the Admin area will be protected with login/pass as defined below.
  ** Set to false for public usage (eg on a restricted intranet or for test setups)
  ** Read http://yourls.org/privatepublic for more details if you're unsure */
-define( 'YOURLS_PRIVATE', true );
+
+define( 'YOURLS_PRIVATE', $_ENV["IS_PRIVATE"] );
 
 /** A random secret hash used to encrypt cookies. You don't have to remember it, make it long and complicated
  ** Hint: copy from http://yourls.org/cookie */
-define( 'YOURLS_COOKIEKEY', 'modify this text with something random' );
+define( 'YOURLS_COOKIEKEY', $_ENV["COOKIE_KEY"] );
 
 /** Username(s) and password(s) allowed to access the site. Passwords either in plain text or as encrypted hashes
  ** YOURLS will auto encrypt plain text passwords in this file
  ** Read http://yourls.org/userpassword for more information */
 $yourls_user_passwords = [
-	'username' => 'password',
+	$_ENV["DEFAULT_USERNAME"] => $_ENV["DEFAULT_PASSWORD"],
 	// 'username2' => 'password2',
 	// You can have one or more 'login'=>'password' lines
 ];
@@ -72,11 +73,11 @@ $yourls_user_passwords = [
  ** 36: generates all lowercase keywords (ie: 13jkm)
  ** 62: generates mixed case keywords (ie: 13jKm or 13JKm) 
  ** For more information, see https://yourls.org/urlconvert */
-define( 'YOURLS_URL_CONVERT', 36 );
+define( 'YOURLS_URL_CONVERT', $_ENV["URL_CONVERT"] );
 
 /** Debug mode to output some internal information
  ** Default is false for live site. Enable when coding or before submitting a new issue */
-define( 'YOURLS_DEBUG', false );
+define( 'YOURLS_DEBUG', $_ENV["IS_DEBUG"] );
 
 /**
 * Reserved keywords (so that generated URLs won't match them)
